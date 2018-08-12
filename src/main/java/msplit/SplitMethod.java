92c5b28f7730a4f33b330549c2c555869e700dd2
Chad Retz <chad.retz@gmail.com>
2018-08-12 08:10:10 -0500

    More work on rebuilding newly split method

--- src/main/java/msplit/SplitMethod.java
+++ src/main/java/msplit/SplitMethod.java
@@ -32,12 +32,12 @@ public class SplitMethod {
       }
     }
     if (largest == null) return null;
-    return fromSplitPoint(method, largest);
+    return fromSplitPoint(owner, method, largest);
   }
 
-  public Result fromSplitPoint(MethodNode orig, Splitter.SplitPoint splitPoint) {
+  public Result fromSplitPoint(String owner, MethodNode orig, Splitter.SplitPoint splitPoint) {
     MethodNode splitOff = createSplitOffMethod(orig, splitPoint);
-    MethodNode trimmed = createTrimmedMethod(orig, splitOff, splitPoint);
+    MethodNode trimmed = createTrimmedMethod(owner, orig, splitOff, splitPoint);
     return new Result(trimmed, splitOff);
   }
 
@@ -66,13 +66,7 @@ public class SplitMethod {
     // First set of instructions is pushing the new stack from the params
     for (int i = 0; i < splitPoint.neededFromStackAtStart.size(); i++) {
       Type item = splitPoint.neededFromStackAtStart.get(i);
-      int op;
-      if (item == Type.INT_TYPE) op = Opcodes.ILOAD;
-      else if (item == Type.LONG_TYPE) op = Opcodes.LLOAD;
-      else if (item == Type.FLOAT_TYPE) op = Opcodes.FLOAD;
-      else if (item == Type.DOUBLE_TYPE) op = Opcodes.DLOAD;
-      else op = Opcodes.ALOAD;
-      newMethod.visitVarInsn(op, i);
+      newMethod.visitVarInsn(loadOpFromType(item), i);
     }
     // Next set of instructions comes verbatim from the original, but we have to change the local indexes
     for (int i = 0; i < splitPoint.length; i++) {
@@ -123,13 +117,7 @@ public class SplitMethod {
       intConst(currIndex).accept(newMethod);
       currIndex++;
       // Load the var
-      int op;
-      if (item == Type.INT_TYPE) op = Opcodes.ILOAD;
-      else if (item == Type.FLOAT_TYPE) op = Opcodes.FLOAD;
-      else if (item == Type.LONG_TYPE) op = Opcodes.LLOAD;
-      else if (item == Type.DOUBLE_TYPE) op = Opcodes.DLOAD;
-      else op = Opcodes.ALOAD;
-      newMethod.visitVarInsn(op, localsMap.get(index));
+      newMethod.visitVarInsn(loadOpFromType(item), localsMap.get(index));
       // Box it if necessary
       boxStackIfNecessary(item, newMethod);
       // Store in array
@@ -138,11 +126,128 @@ public class SplitMethod {
     // Load the array out and return it
     newMethod.visitVarInsn(Opcodes.ALOAD, retArrLocalIndex);
     newMethod.visitInsn(Opcodes.ARETURN);
+    // Any try catch blocks that were completely within we re-add here
+    // TODO: Add try-catch blocks that were completely inside
+    // Reset the labels
+    newMethod.instructions.resetLabels();
     return newMethod;
   }
 
-  protected MethodNode createTrimmedMethod(MethodNode orig, MethodNode splitOff, Splitter.SplitPoint splitPoint) {
-    throw new UnsupportedOperationException("TODO: the rest");
+  protected MethodNode createTrimmedMethod(String owner, MethodNode orig,
+      MethodNode splitOff, Splitter.SplitPoint splitPoint) {
+    // The trimmed method is the same as the original, yet the split area is replaced with a call to the split off
+    // portion. Before calling the split-off, we have to add locals to the stack part. Then afterwards, we have to
+    // replace the stack and written locals.
+    // Effectively clone the orig
+    MethodNode newMethod = new MethodNode(api, orig.access, orig.name, orig.desc,
+        orig.signature, orig.exceptions.toArray(new String[0]));
+    orig.accept(newMethod);
+    // Remove all insns, we'll re-add the ones outside the split range
+    newMethod.instructions.clear();
+    // Remove all try catch blocks, we'll re-add them at the end
+    newMethod.tryCatchBlocks.clear();
+    // Add the insns before split
+    for (int i = 0; i < splitPoint.start; i++) {
+      AbstractInsnNode insn = orig.instructions.get(i + splitPoint.start);
+      // Skip frames
+      if (insn instanceof FrameNode) continue;
+      insn.accept(newMethod);
+    }
+    // Push all the read locals on the stack
+    splitPoint.localsRead.forEach((index, type) -> newMethod.visitVarInsn(loadOpFromType(type), index));
+    // Invoke the split off method
+    newMethod.visitMethodInsn(Opcodes.INVOKESTATIC, owner, splitOff.name, splitOff.desc, false);
+    // Now the object array is on the stack which contains stack pieces + written locals
+    // Take off the locals
+    int localArrIndex = splitPoint.putOnStackAtEnd.size();
+    for (Integer index : splitPoint.localsWritten.keySet()) {
+      // Dupe the array
+      newMethod.visitInsn(Opcodes.DUP);
+      // Put the index on the stack
+      intConst(localArrIndex).accept(newMethod);
+      localArrIndex++;
+      // Load the written local
+      Type item = splitPoint.localsWritten.get(index);
+      newMethod.visitInsn(arrLoadOpFromType(item));
+      // Cast to local type
+      if (!"java/lang/Object".equals(item.getInternalName())) {
+        newMethod.visitTypeInsn(Opcodes.CHECKCAST, boxedTypeIfNecessary(item).getInternalName());
+      }
+      // Unbox if necessary
+      unboxStackIfNecessary(item, newMethod);
+      // Store in the local
+      newMethod.visitVarInsn(storeOpFromType(item), index);
+    }
+    // Now just load up the stack
+    for (int i = 0; i < splitPoint.putOnStackAtEnd.size(); i++) {
+      boolean last = i == splitPoint.putOnStackAtEnd.size() - 1;
+      // Since the loop started with the array, we only dupe the array every time but the last
+      if (!last) newMethod.visitInsn(Opcodes.DUP);
+      // Put the index on the stack
+      intConst(i).accept(newMethod);
+      // Load the stack item
+      Type item = splitPoint.putOnStackAtEnd.get(i);
+      newMethod.visitInsn(arrLoadOpFromType(item));
+      // Cast to local type
+      if (!"java/lang/Object".equals(item.getInternalName())) {
+        newMethod.visitTypeInsn(Opcodes.CHECKCAST, boxedTypeIfNecessary(item).getInternalName());
+      }
+      // Unbox if necessary
+      unboxStackIfNecessary(item, newMethod);
+      // For all but the last stack item, we need to swap with the arr ref above.
+      if (!last) {
+        // Note if the stack item takes two slots, we do a form of dup then pop since there's no swap1x2
+        if (item == Type.LONG_TYPE || item == Type.DOUBLE_TYPE) {
+          newMethod.visitInsn(Opcodes.DUP_X2);
+          newMethod.visitInsn(Opcodes.POP);
+        } else {
+          newMethod.visitInsn(Opcodes.SWAP);
+        }
+      }
+    }
+    // Now we have restored all locals and all stack...add the rest of the insns after the split
+    for (int i = splitPoint.start + splitPoint.length; i < orig.instructions.size(); i++) {
+      AbstractInsnNode insn = orig.instructions.get(i + splitPoint.start);
+      // Skip frames
+      if (insn instanceof FrameNode) continue;
+      insn.accept(newMethod);
+    }
+    // TODO: Add the try-catch blocks
+    // Reset the labels
+    newMethod.instructions.resetLabels();
+    return newMethod;
+  }
+
+  protected static int storeOpFromType(Type type) {
+    if (type == Type.INT_TYPE) return Opcodes.ISTORE;
+    else if (type == Type.LONG_TYPE) return Opcodes.LSTORE;
+    else if (type == Type.FLOAT_TYPE) return Opcodes.FSTORE;
+    else if (type == Type.DOUBLE_TYPE) return Opcodes.DSTORE;
+    else return Opcodes.ASTORE;
+  }
+
+  protected static int loadOpFromType(Type type) {
+    if (type == Type.INT_TYPE) return Opcodes.ILOAD;
+    else if (type == Type.LONG_TYPE) return Opcodes.LLOAD;
+    else if (type == Type.FLOAT_TYPE) return Opcodes.FLOAD;
+    else if (type == Type.DOUBLE_TYPE) return Opcodes.DLOAD;
+    else return Opcodes.ALOAD;
+  }
+
+  protected static int arrLoadOpFromType(Type type) {
+    if (type == Type.INT_TYPE) return Opcodes.IALOAD;
+    else if (type == Type.LONG_TYPE) return Opcodes.LALOAD;
+    else if (type == Type.FLOAT_TYPE) return Opcodes.FALOAD;
+    else if (type == Type.DOUBLE_TYPE) return Opcodes.DALOAD;
+    else return Opcodes.AALOAD;
+  }
+
+  protected static Type boxedTypeIfNecessary(Type type) {
+    if (type == Type.INT_TYPE) return Type.getType(Integer.class);
+    else if (type == Type.LONG_TYPE) return Type.getType(Long.class);
+    else if (type == Type.FLOAT_TYPE) return Type.getType(Float.class);
+    else if (type == Type.DOUBLE_TYPE) return Type.getType(Double.class);
+    else return type;
   }
 
   protected static void boxStackIfNecessary(Type type, MethodNode method) {
@@ -150,6 +255,17 @@ public class SplitMethod {
     else if (type == Type.FLOAT_TYPE) boxCall(Float.class, type).accept(method);
     else if (type == Type.LONG_TYPE) boxCall(Long.class, type).accept(method);
     else if (type == Type.DOUBLE_TYPE) boxCall(Double.class, type).accept(method);
+  }
+
+  protected static void unboxStackIfNecessary(Type type, MethodNode method) {
+    if (type == Type.INT_TYPE) method.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+        "java/lang/Integer", "intValue", Type.getMethodDescriptor(Type.INT_TYPE), false);
+    else if (type == Type.FLOAT_TYPE) method.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+        "java/lang/Float", "floatValue", Type.getMethodDescriptor(Type.FLOAT_TYPE), false);
+    else if (type == Type.LONG_TYPE) method.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+        "java/lang/Long", "longValue", Type.getMethodDescriptor(Type.LONG_TYPE), false);
+    else if (type == Type.DOUBLE_TYPE) method.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+        "java/lang/Double", "doubleValue", Type.getMethodDescriptor(Type.DOUBLE_TYPE), false);
   }
 
   protected static AbstractInsnNode intConst(int v) {
